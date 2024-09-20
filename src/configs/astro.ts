489e14853d8ef9775ad5a1530f39ab01c383224f
javalce <javalce29@gmail.com>
2024-09-20 18:28:08 +0200

    refactor: simplify Astro ESLint configuration by removing unnecessary logic

--- src/configs/astro.ts
+++ src/configs/astro.ts
@@ -3,18 +3,8 @@ import eslintPluginAstro from 'eslint-plugin-astro';
 import { type TypedConfigItem } from '../types';
 
 export function astro(): TypedConfigItem[] {
-  const config = [
+  return [
     ...eslintPluginAstro.configs['flat/recommended'],
     ...eslintPluginAstro.configs['flat/jsx-a11y-recommended'],
   ];
-
-  const jsxA11yConfig = config.find((item) => item.name === undefined) ?? {};
-  const index = config.indexOf(jsxA11yConfig);
-
-  config.splice(index, 1, {
-    name: 'astro/jsx-a11y',
-    ...jsxA11yConfig,
-  });
-
-  return [...new Set(config)];
 }
