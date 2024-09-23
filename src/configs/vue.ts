397da1cc0c1c3030e28e7aa938e370654c306d26
javalce <javalce29@gmail.com>
2024-09-23 19:14:55 +0200

    feat: add custom Vue rules and configuration for improved linting

--- src/configs/vue.ts
+++ src/configs/vue.ts
@@ -16,6 +16,81 @@ export async function vue({
     ...((version === 3
       ? pluginVue.configs['flat/recommended']
       : pluginVue.configs['flat/vue2-recommended']) as TypedConfigItem[]),
+    {
+      name: 'javalce/vue',
+      files: [VUE_FILES],
+      languageOptions: {
+        globals: {},
+      },
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        'vue/block-order': [
+          'error',
+          {
+            order: ['script', 'template', 'style'],
+          },
+        ],
+        'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+        'vue/component-options-name-casing': ['error', 'PascalCase'],
+        // this is deprecated
+        'vue/component-tags-order': 'off',
+        'vue/custom-event-name-casing': ['error', 'camelCase'],
+        'vue/define-macros-order': [
+          'error',
+          {
+            order: ['defineOptions', 'defineProps', 'defineEmits', 'defineSlots'],
+          },
+        ],
+        'vue/dot-location': ['error', 'property'],
+        'vue/dot-notation': ['error', { allowKeywords: true }],
+        'vue/eqeqeq': ['error', 'smart'],
+        'vue/max-attributes-per-line': 'off',
+        'vue/multi-word-component-names': 'off',
+        'vue/no-dupe-keys': 'off',
+        'vue/no-empty-pattern': 'error',
+        'vue/no-irregular-whitespace': 'error',
+        'vue/no-loss-of-precision': 'error',
+        'vue/no-restricted-syntax': [
+          'error',
+          'DebuggerStatement',
+          'LabeledStatement',
+          'WithStatement',
+        ],
+        'vue/no-restricted-v-bind': ['error', '/^v-/'],
+        'vue/no-setup-props-reactivity-loss': 'off',
+        'vue/no-sparse-arrays': 'error',
+        'vue/no-unused-refs': 'error',
+        'vue/no-useless-v-bind': 'error',
+        'vue/no-v-html': 'off',
+        'vue/object-shorthand': [
+          'error',
+          'always',
+          {
+            avoidQuotes: true,
+            ignoreConstructors: false,
+          },
+        ],
+        'vue/prefer-separate-static-class': 'error',
+        'vue/prefer-template': 'error',
+        'vue/prop-name-casing': ['error', 'camelCase'],
+        'vue/require-default-prop': 'off',
+        'vue/require-prop-types': 'off',
+        'vue/space-infix-ops': 'error',
+        'vue/space-unary-ops': ['error', { nonwords: false, words: true }],
+        'vue/html-self-closing': [
+          'warn',
+          {
+            html: { void: 'always', normal: 'always', component: 'always' },
+            math: 'always',
+            svg: 'always',
+          },
+        ],
+        'vue/singleline-html-element-content-newline': 'off',
+        'vue/html-closing-bracket-newline': 'off',
+        'vue/html-quotes': 'off',
+        'vue/html-indent': 'off',
+      },
+    },
   ];
 
   if (typescript) {
