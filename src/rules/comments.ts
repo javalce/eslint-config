c84200949588b0c756d4129aefc64e9f86788858
javalce <javalce29@gmail.com>
2024-09-23 13:44:54 +0200

    build: update ESLint plugin dependencies and import path for eslint-comments

--- src/rules/comments.ts
+++ src/rules/comments.ts
@@ -1,6 +1,6 @@
 import { type ESLint } from 'eslint';
 // @ts-expect-error - ESLint plugin import is not resolved correctly
-import eslintCommentsPlugin from 'eslint-plugin-eslint-comments';
+import eslintCommentsPlugin from '@eslint-community/eslint-plugin-eslint-comments';
 
 import { type TypedConfigItem } from '../types';
 
