dd0b3283c42ec29fb97893e85669071b85fc2b61
Javier Valero Cejudo <javalce29@gmail.com>
2025-01-17 19:29:40 +0100

    refactor: enhance init command with framework and testing options

--- src/cli/commands/init.ts
+++ src/cli/commands/init.ts
@@ -3,11 +3,17 @@ import type { Config, Framework, TestingFramework } from '../types';
 import fs from 'node:fs/promises';
 
 import chalk from 'chalk';
-import { Command } from 'commander';
+import { Command, createOption } from 'commander';
 import ora from 'ora';
 import prompts from 'prompts';
 
-import { DEPENDENCIES_MAP, FRAMEWORK_OPTIONS, TESTING_FRAMEWORK_OPTIONS } from '../constants';
+import {
+  DEPENDENCIES_MAP,
+  FRAMEWORK_OPTIONS,
+  FRAMEWORKS,
+  TESTING_FRAMEWORK_OPTIONS,
+  TESTING_FRAMEWORKS,
+} from '../constants';
 import { handleError } from '../utils/handle-error';
 import { logger } from '../utils/logger';
 import { installDependencies, isPackageTypeModule } from '../utils/npm-utils';
@@ -15,91 +21,110 @@ import { installDependencies, isPackageTypeModule } from '../utils/npm-utils';
 export const init = new Command()
   .name('init')
   .description('Create a new ESLint config in your project')
-  .action(async () => {
-    try {
-      const frameworkOptions: Omit<Config, 'type'> = await prompts(
-        [
-          {
-            type: 'select',
-            name: 'framework',
-            message: 'Select the framework want you use',
-            choices: FRAMEWORK_OPTIONS,
-            initial: 0,
-          },
-          {
-            type: 'select',
-            name: 'testing',
-            message: 'Select the testing framework you use',
-            choices: TESTING_FRAMEWORK_OPTIONS,
-            initial: 0,
-          },
-        ],
-        {
-          onCancel: () => {
-            logger.error('Command aborted');
-            process.exit(1);
-          },
-        },
-      );
+  .addOption(
+    createOption('-f, --framework <framework>', 'The framework you want to use').choices(
+      Object.values(FRAMEWORKS),
+    ),
+  )
+  .addOption(
+    createOption('-t, --[no]-testing <testing>', 'The testing framework you want to use').choices(
+      Object.values(TESTING_FRAMEWORKS),
+    ),
+  )
+  .addOption(createOption('--no-testing', 'Skip the testing framework selection').default(false))
+  .addOption(createOption('--lib', 'Create a config for a library project').default(false))
+  .action(
+    async (args: {
+      framework?: Framework;
+      testing?: TestingFramework | null | boolean;
+      lib: boolean;
+    }) => {
+      const framework = args.framework ?? (await getFrameworkSelection());
+      const testing =
+        args.testing === false
+          ? null
+          : ((args.testing as TestingFramework) ?? (await getTestingFrameworkSelection()));
 
-      const typeOptions: Pick<Config, 'type'> = await prompts(
-        {
-          type: () => (frameworkOptions.framework === null ? 'select' : null),
-          name: 'type',
-          message: 'Select the type of project',
-          choices: [
-            { title: 'Library', value: 'lib' },
-            { title: 'Application', value: 'app' },
-          ],
-          initial: 1,
-        },
-        {
-          onCancel: () => {
-            logger.error('Command aborted');
-            process.exit(1);
-          },
-        },
-      );
-
-      const options = {
-        ...frameworkOptions,
-        ...typeOptions,
+      const options: Config = {
+        framework,
+        testing,
+        lib: args.lib,
       } as Config;
 
-      const { framework, testing } = options;
+      try {
+        // Get the dependencies required for the selected config
+        const deps = getDependencies(framework, testing);
 
-      // Get the dependencies required for the selected config
-      const deps = getDependencies(framework, testing);
+        // Display the dependencies
+        logger.info("The config that you've selected requires the following dependencies:");
+        logger.break();
+        logger.log(chalk.blue(deps.join(' ')));
+        logger.break();
 
-      // Display the dependencies
-      logger.info("The config that you've selected requires the following dependencies:");
-      logger.break();
-      logger.log(chalk.blue(deps.join(' ')));
-      logger.break();
+        const { confirm } = (await prompts({
+          type: 'confirm',
+          name: 'confirm',
+          message: 'Do you want to install these dependencies?',
+          initial: true,
+        })) as { confirm: boolean };
 
-      const { confirm } = (await prompts({
-        type: 'confirm',
-        name: 'confirm',
-        message: 'Do you want to install these dependencies?',
-        initial: true,
-      })) as { confirm: boolean };
+        if (confirm) {
+          await installDependencies(deps);
+        }
 
-      if (confirm) {
-        await installDependencies(deps);
+        // Write the ESLint config
+        await writeEslintConfig(options);
+      } catch (error) {
+        handleError(error);
       }
+    },
+  );
 
-      // Write the ESLint config
-      await writeEslintConfig(options);
-    } catch (error) {
-      handleError(error);
-    }
-  });
+async function getTestingFrameworkSelection(): Promise<TestingFramework | null> {
+  const { testing } = await prompts(
+    {
+      type: 'select',
+      name: 'testing',
+      message: 'Select the testing framework you use',
+      choices: TESTING_FRAMEWORK_OPTIONS,
+      initial: 0,
+    },
+    {
+      onCancel: () => {
+        logger.error('Command aborted');
+        process.exit(1);
+      },
+    },
+  );
 
-function getDependencies(framework: Framework | null, testing: TestingFramework | null): string[] {
+  return testing;
+}
+
+async function getFrameworkSelection(): Promise<Framework> {
+  const { framework } = await prompts(
+    {
+      type: 'select',
+      name: 'framework',
+      message: 'Select the framework want you use',
+      choices: FRAMEWORK_OPTIONS,
+      initial: 0,
+    },
+    {
+      onCancel: () => {
+        logger.error('Command aborted');
+        process.exit(1);
+      },
+    },
+  );
+
+  return framework;
+}
+
+function getDependencies(framework: Framework, testing: TestingFramework | null): string[] {
   const spinner = ora('Collecting dependencies...').start();
   const deps = new Set<string>(['eslint', '@javalce/eslint-config']);
 
-  if (framework) {
+  if (framework !== 'node') {
     DEPENDENCIES_MAP[framework].forEach((dep) => deps.add(dep));
   }
 
@@ -116,7 +141,7 @@ function getDependencies(framework: Framework | null, testing: TestingFramework 
   return Array.from(deps);
 }
 
-async function writeEslintConfig({ framework, testing, type: projectType }: Config): Promise<void> {
+async function writeEslintConfig({ framework, testing, lib }: Config): Promise<void> {
   const isESMModule = await isPackageTypeModule();
   const configFilename = isESMModule ? 'eslint.config.js' : 'eslint.config.mjs';
 
@@ -125,7 +150,7 @@ async function writeEslintConfig({ framework, testing, type: projectType }: Conf
 export default defineConfig({
 `;
 
-  if (framework) {
+  if (framework !== 'node') {
     if (['react', 'vue', 'solid'].includes(framework) && framework !== 'next') {
       config += `  typescript: ['tsconfig.node.json', 'tsconfig.app.json'],\n`;
     }
@@ -137,8 +162,8 @@ export default defineConfig({
     config += `  testing: '${testing}',\n`;
   }
 
-  if (projectType === 'lib') {
-    config += `  type: '${projectType}'\n`;
+  if (lib) {
+    config += `  type: 'lib'\n`;
   }
   config += '});';
 
