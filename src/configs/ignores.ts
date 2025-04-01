86d11027d60325d039d075b6557212d124fbdb38
Javier Valero Cejudo <javalce29@gmail.com>
2025-04-01 19:12:51 +0200

    refactor: remove eslint-config-flat-gitignore and update ignores configuration

--- src/configs/ignores.ts
+++ src/configs/ignores.ts
@@ -1,18 +1,30 @@
-import gitignore from 'eslint-config-flat-gitignore';
+import path from 'node:path';
 
+import { includeIgnoreFile } from '@eslint/compat';
+import { globalIgnores } from 'eslint/config';
+
+import fs from 'node:fs';
 import { type TypedConfigItem } from '../types';
 
 export function ignores({ files }: { files?: string[] }): TypedConfigItem[] {
-  return [
-    {
-      ...gitignore({ strict: false }),
+  const cwd = process.cwd();
+  const gitignorePath = path.join(cwd, '.gitignore');
+
+  const config: TypedConfigItem[] = [];
+
+  if (fs.existsSync(gitignorePath)) {
+    config.push({
+      ...includeIgnoreFile(gitignorePath),
       name: 'javalce/gitignore',
-    },
-    files
-      ? {
-          ignores: files,
-          name: 'javalce/ignores',
-        }
-      : {},
-  ];
+    });
+  }
+
+  if (files) {
+    config.push({
+      ...globalIgnores(files),
+      name: 'javalce/ignores',
+    });
+  }
+
+  return config;
 }
