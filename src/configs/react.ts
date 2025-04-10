56522dc6a3b4d51d3d7d69d233df054d1332a286
Javier Valero Cejudo <javalce29@gmail.com>
2025-04-10 20:14:55 +0200

    feat: add eslint-plugin-react-refresh and integrate with react configuration

--- src/configs/react.ts
+++ src/configs/react.ts
@@ -2,21 +2,35 @@ import { SRC_FILES } from '../constants';
 import jsxA11Rules from '../rules/jsx-a11y';
 import reactRules from '../rules/react';
 import { type TypedConfigItem } from '../types';
-import { lazy } from '../utils';
+import { hasPackage, lazy } from '../utils';
+
+const ReactRouterPackages = [
+  '@react-router/node',
+  '@react-router/react',
+  '@react-router/serve',
+  '@react-router/dev',
+];
+const NextJsPackages = ['next'];
 
 export async function react(): Promise<TypedConfigItem[]> {
-  const [reactPlugin, reactHooksPlugin, jsxA11yPlugin, eslintPluginImport] = await Promise.all([
-    lazy(import('eslint-plugin-react')),
-    lazy(import('eslint-plugin-react-hooks')),
-    lazy(import('eslint-plugin-jsx-a11y')),
-    lazy(import('eslint-plugin-import-x')),
-  ]);
+  const [reactPlugin, reactHooksPlugin, reactRefreshPlugin, jsxA11yPlugin, eslintPluginImport] =
+    await Promise.all([
+      lazy(import('eslint-plugin-react')),
+      lazy(import('eslint-plugin-react-hooks')),
+      lazy(import('eslint-plugin-react-refresh')),
+      lazy(import('eslint-plugin-jsx-a11y')),
+      lazy(import('eslint-plugin-import-x')),
+    ]);
+
+  const isUsingReactRouter = ReactRouterPackages.some((pkg) => hasPackage(pkg));
+  const isUsingNextJs = NextJsPackages.some((pkg) => hasPackage(pkg));
 
   return [
     {
       plugins: {
         react: reactPlugin,
         'react-hooks': reactHooksPlugin,
+        'react-refresh': reactRefreshPlugin,
         'jsx-a11y': jsxA11yPlugin,
       },
       name: 'react/setup',
@@ -36,6 +50,38 @@ export async function react(): Promise<TypedConfigItem[]> {
         rules: {
           ...reactPlugin.configs.flat.recommended.rules,
           ...reactHooksPlugin.configs.recommended.rules,
+          'react-refresh/only-export-components': [
+            'warn',
+            {
+              allowConstantExport: true,
+              allowExportNames: [
+                ...(() => {
+                  if (isUsingNextJs) {
+                    return [
+                      'dynamic',
+                      'dynamicParams',
+                      'revalidate',
+                      'fetchCache',
+                      'runtime',
+                      'preferredRegion',
+                      'maxDuration',
+                      'config',
+                      'generateStaticParams',
+                      'metadata',
+                      'generateMetadata',
+                      'viewport',
+                    ];
+                  }
+
+                  if (isUsingReactRouter) {
+                    return ['meta', 'links', 'headers', 'loader', 'action'];
+                  }
+
+                  return [];
+                })(),
+              ],
+            },
+          ],
           ...jsxA11yPlugin.flatConfigs.recommended.rules,
         },
         name: 'react/rules',
