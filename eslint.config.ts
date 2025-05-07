6d8d0de140ea72a87c92d3bc030645f5e93a756f
Javier Valero Cejudo <javalce29@gmail.com>
2025-05-07 19:47:51 +0200

    chore: enable no-default-export rule in ESLint configuration

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from './src/index';
 
 export default defineConfig({
+  react: true,
   type: 'lib',
   overrides: [
     {
       files: ['src/rules/**/*.ts'],
       rules: {
         'sort-keys': 'error',
+        'import-x/no-default-export': 'off',
       },
     },
   ],
