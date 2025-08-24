07eec8f98ec4d1bbee551af9cb023dbc608518f7
Javier Valero Cejudo <javalce29@gmail.com>
2025-08-24 21:51:03 +0200

    feat: drop commonjs compatibility

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,9 +3,8 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts', 'src/globs.ts'],
   shims: true,
-  format: ['esm', 'cjs'],
+  format: ['esm'],
   clean: true,
   dts: true,
-  cjsInterop: true,
   minify: true,
 });
