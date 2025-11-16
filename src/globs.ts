950f0e73fa1b705d6f9a9af0bd20d2f6635c1b8c
Javier Valero Cejudo <javalce29@gmail.com>
2025-11-16 13:32:45 +0100

    style: reorganize glob constants for better clarity and maintainability

--- src/globs.ts
+++ src/globs.ts
@@ -1,9 +1,10 @@
-export const GLOB_JS_FILES = '**/*.?([cm])js';
+export const GLOB_SRC_FILES_EXT = '?([cm])[jt]s?(x)';
+export const GLOB_SRC_FILES = '**/*.?([cm])[jt]s?(x)';
 
+export const GLOB_JS_FILES = '**/*.?([cm])js';
 export const GLOB_JSX_FILES = '**/*.?([cm])jsx';
 
 export const GLOB_TS_FILES = '**/*.?([cm])ts';
-
 export const GLOB_TSX_FILES = '**/*.?([cm])tsx';
 
 export const GLOB_SVELTE_FILES = '**/*.svelte';
@@ -11,16 +12,10 @@ export const GLOB_SVELTE_FILES = '**/*.svelte';
 export const GLOB_VUE_FILES = '**/*.vue';
 
 export const GLOB_ASTRO_FILES = ['*.astro', '**/*.astro'];
-
 export const GLOB_ASTRO_JS_FILES = ['**/*.astro/*.js', '*.astro/*.js'];
-
 export const GLOB_ASTRO_TS_FILES = ['**/*.astro/*.ts', '*.astro/*.ts'];
 
 export const GLOB_HTML_FILES = '**/*.html';
-
-export const GLOB_SRC_FILES_EXT = '?([cm])[jt]s?(x)';
-
-export const GLOB_SRC_FILES = '**/*.?([cm])[jt]s?(x)';
 
 const GLOB_SRC_FILES_TS_EXT = '?([cm])ts?(x)';
 
@@ -31,7 +26,6 @@ export const GLOB_TEST_FILES = [
   `**/*.bench.${GLOB_SRC_FILES_EXT}`,
   `**/*.benchmark.${GLOB_SRC_FILES_EXT}`,
 ];
-
 export const GLOB_TS_TEST_FILES = [
   `**/__tests__/**/*.${GLOB_SRC_FILES_TS_EXT}`,
   `**/*.spec.${GLOB_SRC_FILES_TS_EXT}`,
