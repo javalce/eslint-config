5d5d14c5f4f876b43e2331715abde7d6d9388eb1
Javier Valero Cejudo <javalce29@gmail.com>
2026-06-15 18:59:08 +0200

    refactor: move stylistic rules for js from stylictic to javascript config

--- src/configs/javascript.ts
+++ src/configs/javascript.ts
@@ -34,6 +34,68 @@ export function javascript({ ecmaVersion = 2023, overrides }: OptionsJavascript 
     posibleErrorsConfig,
     variablesConfig,
     {
+      name: 'javascript/rules/stylistic',
+      rules: {
+        /**
+         * Require camel case names.
+         *
+         * 🚫 Not fixable - https://eslint.org/docs/rules/camelcase
+         */
+        camelcase: [
+          'error',
+          { allow: ['^UNSAFE_'], ignoreDestructuring: false, properties: 'never' },
+        ],
+        /**
+         * Require function expressions to have a name.
+         *
+         * 🚫 Not fixable - https://eslint.org/docs/rules/func-names
+         */
+        'func-names': ['error', 'as-needed'],
+        /**
+         * Require a capital letter for constructors.
+         *
+         * 🚫 Not fixable - https://eslint.org/docs/rules/new-cap
+         */
+        'new-cap': ['error', { newIsCap: true, capIsNew: false, properties: true }],
+        /**
+         * Disallow use of the Array constructor.
+         *
+         * 🚫 Not fixable - https://eslint.org/docs/rules/no-array-constructor
+         */
+        'no-array-constructor': 'error',
+        /**
+         * Disallow if as the only statement in an else block.
+         *
+         * 🔧 Fixable - https://eslint.org/docs/rules/no-lonely-if
+         */
+        'no-lonely-if': 'warn',
+        /**
+         * Disallow use of chained assignment expressions.
+         *
+         * 🚫 Not fixable - https://eslint.org/docs/rules/no-multi-assign
+         */
+        'no-multi-assign': ['error'],
+        /**
+         * Disallow nested ternary expressions.
+         *
+         * 🚫 Not fixable - https://eslint.org/docs/rules/no-nested-ternary
+         */
+        'no-nested-ternary': 'error',
+        /**
+         * Disallow ternary operators when simpler alternatives exist.
+         *
+         * 🚫 Not fixable - https://eslint.org/docs/rules/no-unneeded-ternary
+         */
+        'no-unneeded-ternary': ['error', { defaultAssignment: false }],
+        /**
+         * Require use of an object spread over Object.assign.
+         *
+         * 🔧 Fixable - https://eslint.org/docs/rules/prefer-object-spread
+         */
+        'prefer-object-spread': 'warn',
+      },
+    },
+    {
       name: 'javascript/rules/overrides',
       rules: {
         ...overrides,
