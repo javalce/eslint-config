9b13fcff18af504654449d2ac17051aa7471a220
Javier Valero Cejudo <javalce29@gmail.com>
2026-09-20 17:58:22 +0200

    refactor: consolidate Vitest stylistic rules into vitest function

--- src/configs/vitest.ts
+++ src/configs/vitest.ts
@@ -1,7 +1,6 @@
 import type { Config, OptionsHasTypescript, OptionsVitest } from '../types';
 
 import { GLOB_TEST_FILES } from '../globs';
-import eslintConfigVitest from '../rules/vitest';
 import { ensureInstalled, resolveDefaultExport } from '../utils';
 
 export async function vitest({
@@ -40,7 +39,42 @@ export async function vitest({
       },
       name: 'vitest/rules',
     },
-    eslintConfigVitest,
+    {
+      files: GLOB_TEST_FILES,
+      name: 'vitest/rules/stylistic',
+      rules: {
+        /**
+         * It's not important to know the return type of functions in tests.
+         *
+         * 🚫 Not fixable - https://typescript-eslint.io/rules/explicit-function-return-type/
+         */
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        /**
+         * Disallow use of `test` and `it` in the same file. Use only `it`.
+         *
+         * 🚫 Not fixable - https://github.com/vitest-dev/eslint-plugin-vitest/blob/HEAD/docs/rules/consistent-test-it.md
+         */
+        'vitest/consistent-test-it': ['error', { fn: 'it', withinDescribe: 'it' }],
+        /**
+         * Disallow duplicate setup and teardown hooks.
+         *
+         * 🚫 Not fixable - https://github.com/vitest-dev/eslint-plugin-vitest/blob/HEAD/docs/rules/no-duplicate-hooks.md
+         */
+        'vitest/no-duplicate-hooks': 'error',
+        /**
+         * Enforce having hooks in consistent order.
+         *
+         * 🚫 Not fixable - https://github.com/vitest-dev/eslint-plugin-vitest/blob/HEAD/docs/rules/prefer-hooks-in-order.md
+         */
+        'vitest/prefer-hooks-in-order': 'error',
+        /**
+         * Require lowercase test names.
+         *
+         * 🔧 Fixable - https://github.com/vitest-dev/eslint-plugin-vitest/blob/HEAD/docs/rules/prefer-lowercase-title.md
+         */
+        'vitest/prefer-lowercase-title': ['warn', { ignoreTopLevelDescribe: true }],
+      },
+    },
     {
       files: GLOB_TEST_FILES,
       rules: {
